dbfe9713ca31d9de89bd767f5740fda9ed1a5710
Eslem <practicas_informatic@INFORMATICA005.sermesa.local>
2014-05-20 13:26:41 +0200

    Test script (remove)

--- pullGit.bat
+++
@@ -1,2 +0,0 @@
-call "C:\Program Files (x86)\Git\cmd\git.exe" pull origin master
-call "C:\Archivos de programa\Git\cmd\git.exe" pull origin master
--- uploadGit.bat
+++
@@ -1,6 +0,0 @@
-call "C:\Program Files (x86)\Git\cmd\git.exe" add .
-call "C:\Program Files (x86)\Git\cmd\git.exe" commit -m "automatic Commit %date%"
-call "C:\Program Files (x86)\Git\cmd\git.exe" push -u origin gh-pages
-call "C:\Archivos de programa\Git\cmd\git.exe" add .
-call "C:\Archivos de programa\Git\cmd\git.exe" commit -m "automatic Commit %date%"
-call "C:\Archivos de programa\Git\cmd\git.exe" push -u origin gh-pages

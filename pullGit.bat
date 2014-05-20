589132423a16eda70041bb959059568688da485f
unknown <practicas_informatic@INFORMATICA005.sermesa.local>
2014-05-20 13:04:49 +0200

    automatic Commit 20/05/2014

---
+++ pullGit.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Git\cmd\git.exe" pull origin master
+call "C:\Archivos de programa\Git\cmd\git.exe" pull origin master
---
+++ uploadGit.bat
@@ -0,0 +1,6 @@
+call "C:\Program Files (x86)\Git\cmd\git.exe" add .
+call "C:\Program Files (x86)\Git\cmd\git.exe" commit -m "automatic Commit %date%"
+call "C:\Program Files (x86)\Git\cmd\git.exe" push -u origin master
+call "C:\Archivos de programa\Git\cmd\git.exe" add .
+call "C:\Archivos de programa\Git\cmd\git.exe" commit -m "automatic Commit %date%"
+call "C:\Archivos de programa\Git\cmd\git.exe" push -u origin master

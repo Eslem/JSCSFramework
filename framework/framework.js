23eba0da5407139317c364c85fe03b95b26bcd92
Eslem Alzate <j.eslem03@gmail.com>
2014-06-24 16:34:17 +0200

    XSL Functions

--- framework/framework.js
+++ framework/framework.js
@@ -895,3 +895,40 @@ function scrollTo(){
 		});
 	});
 }
+
+
+//---XSL Function
+
+function loadXMLDoc(filename)
+{
+	if (window.ActiveXObject)
+	{
+		xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+	}
+	else
+	{
+		xhttp = new XMLHttpRequest();
+	}
+	xhttp.open("GET", filename, false);
+	try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
+	xhttp.send("");
+	return xhttp.responseXML;
+}
+function XMLyXSL(xml, xslFile, elem){
+
+	xsl = loadXMLDoc(xslFile);
+	// code for IExpl
+	if (window.ActiveXObject || xhttp.responseType == "msxml-document")
+	{
+		ex = xml.transformNode(xsl);
+		document.getElementById(elem).innerHTML = ex;
+	}
+	// code for Chrome, Firefox, Opera, etc.
+	else if (document.implementation && document.implementation.createDocument)
+	{
+		xsltProcessor = new XSLTProcessor();
+		xsltProcessor.importStylesheet(xsl);
+		resultDocument = xsltProcessor.transformToFragment(xml, document);
+		document.getElementById(elem).appendChild(resultDocument);
+	}
+}
